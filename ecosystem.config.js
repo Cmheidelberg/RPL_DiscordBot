6f276e6f5a14e1e50bbc0bda63ee57cc6e663c43
Ubuntu <ubuntu@ip-172-31-57-110.us-west-2.compute.internal>
2019-08-21 00:04:13 +0000

    Added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'Roles Bot',
+        script: 'python3',
+        cwd: './',
+	interpreter_args: '-u',
+        args: 'rolesBot_1.0.1.py'
+      }
+    ]
+}
